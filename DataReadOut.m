7360ab49720677b9ccfc13c776a9dcbe021fdd3f
RHWvDijk <45235835+RHWvDijk@users.noreply.github.com>
2018-11-25 13:59:14 +0100

    Add files via upload

--- DataReadOut.m
+++ DataReadOut.m
@@ -1,50 +1,120 @@
-% Loading of the data in the form of structs
-load('echodata.mat')
-load('psadata.mat')
-load('dbcdata.mat')
-load('bioptdata.mat')
+function [PSA,MRI,BIOPT,ECHO,DBC] = DataReadOut(datalocation)
+% datalocation should be a string of the folder location where the csv data
+% files are saved. 
+% This document reads out the 5 data files and assigns the columns
+% corresponding to different kinds of data to column vectors. The output 
+% contains 5 structs: PSA,MRI,BIOPT,ECHO,DBC. The strings in the data
+% are converted to numerical values so that they can easily be used in
+% computation later on. 
+
+% Give error when input is not a string
+% if ~isstring(datalocation)
+%    error('Error. \nInput must be a string, not a %s.',class(datalocation))
+% end
 
 %% PSA data Read-out
-psaID = psadata.patientID; 
-age = psadata.Leeftijd;
-psa = psadata.PSA;      % unit is microgram/L
-freepsa = psadata.VRIJPSA;
-psadate = psadata.datumtijd; 
-psadate = datenum(psadate); % Converting of date to a numeric value
-
-
-%% MRI data Read-out --> ik heb deze nu anders gedaan omdat dit er voor zorgt 
-% dat er geen NaN's verschijnen in de data
 % Initialize variables.
-filename = 'C:\Users\s165635\Desktop\DBL Computational Biology (8QC00)\OGO groep 5\OGO PSA - MRI data.csv';
+filename = strcat(datalocation,'\OGO PSA - PSA waarde data.csv');
 delimiter = ',';
 startRow = 2;
+
 % Format string for each line of text:
 %   column1: double (%f)
 %	column2: text (%q)
-%   column3: text (%q)
+%   column3: double (%f)
 %	column4: text (%q)
 %   column5: text (%q)
+%	column6: text (%q)
+%   column7: text (%q)
+%	column8: text (%q)
+%   column9: text (%q)
 % For more information, see the TEXTSCAN documentation.
-formatSpec = '%f%q%q%q%q%[^\n\r]';
+formatSpec = '%f%q%f%q%q%q%q%q%q%[^\n\r]';
+
 % Open the text file.
 fileID = fopen(filename,'r');
+
 % Read columns of data according to format string.
 % This call is based on the structure of the file used to generate this
 % code. If an error occurs for a different file, try regenerating the code
 % from the Import Tool.
 dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN,'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
 % Close the text file.
 fclose(fileID);
 
 % Allocate imported array to column variable names
+psaID = dataArray{:, 1};
+gender = dataArray{:, 2};
+age = dataArray{:, 3};
+psa = dataArray{:, 4};
+freepsa = dataArray{:, 5};
+unit = dataArray{:, 6};
+datepsa = dataArray{:, 7};
+specialism = dataArray{:, 8};
+activity = dataArray{:, 9};
+
+% Clear temporary variables
+clearvars filename delimiter startRow formatSpec fileID dataArray ans;
+
+datepsa = datenum(datepsa); % Converting of date to a numeric value
+
+% PSA values completion
+for i=1:length(psa)
+    if strcmp(psa(i,1),'<0,0030') == 1 
+        psa(i,1) = {'0.0030'}; 
+    elseif strcmp(psa(i,1),'<0,0031')== 1
+        psa(i,1) = {'0.0031'}; 
+    elseif strcmp(psa(i,1),'<0,10') == 1
+        psa(i,1) = {'0.10'}; 
+    elseif strcmp(psa(i,1),'>100') == 1
+        psa(i,1) = {'100'}; 
+    elseif strcmp(psa(i,1),'>5000') == 1
+        psa(i,1) = {'5000'}; 
+    end
+end
+psa = str2double(psa);
+
+
+% FreePSA values completion
+for i=1:length(freepsa)
+    if strcmp(freepsa(i,1),'<0,10') == 1 
+        freepsa(i,1) = {'0.10'}; 
+    elseif strcmp(freepsa(i,1),'>50') == 1
+        freepsa(i,1) = {'50'}; 
+    elseif strcmp(freepsa(i,1),'NA') == 1
+        freepsa(i,1) = {'0'};
+    end
+end
+freepsa = str2double(freepsa);
+
+% Creating a struct of the data
+PSA.ID = psaID;
+PSA.gender = gender;
+PSA.age = age;
+PSA.psa = psa;
+PSA.freepsa = freepsa; 
+PSA.date = datepsa;
+
+%% MRI data Read-out 
+filename = strcat(datalocation,'\OGO PSA - MRI data.csv');
+delimiter = ',';
+startRow = 2;
+
+formatSpec = '%f%q%q%q%q%[^\n\r]';
+
+fileID = fopen(filename,'r');
+
+dataArray = textscan(fileID, formatSpec, 'Delimiter', delimiter, 'EmptyValue' ,NaN,'HeaderLines' ,startRow-1, 'ReturnOnError', false);
+
+fclose(fileID);
+
 mriID = dataArray{:, 1};
 mridate = dataArray{:, 2};
 %OMSCHR = dataArray{:, 3};  deze is waarschijnlijk niet nodig
 pirads = dataArray{:, 4};
 mriPCa = dataArray{:, 5};
 
-% Clear temporary variables
 clearvars filename delimiter startRow formatSpec fileID dataArray ans;
 
 %% MRI data manipulations
@@ -66,8 +136,14 @@ pirads = str2double(pirads);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Creating a struct of the data
+MRI.ID = mriID; 
+MRI.date = mridate; 
+MRI.pirads = pirads;
+MRI.PCa = mriPCa;
+
 %% Biopt data Read-out
-filename = 'C:\Users\s165635\Desktop\DBL Computational Biology (8QC00)\OGO groep 5\OGO PSA - biopt data.csv';
+filename = strcat(datalocation, '\OGO PSA - biopt data.csv');
 delimiter = ',';
 startRow = 2;
 
@@ -116,8 +192,15 @@ end
 bioptPCa = str2double(bioptPCa);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Creating a struct of the data
+BIOPT.ID = bioptID;
+BIOPT.date = bioptdate; 
+BIOPT.PCa = bioptPCa;
+BIOPT.gleason = gleason;
+
+
 %% Echo data Read-out
-filename = 'C:\Users\s165635\Desktop\DBL Computational Biology (8QC00)\OGO groep 5\OGO PSA - Echo data.csv';
+filename = strcat(datalocation, '\OGO PSA - Echo data.csv');
 delimiter = ',';
 startRow = 2;
 
@@ -150,9 +233,13 @@ for i=1:length(volume)
 end
 volume = str2double(volume);
 
+% Creating a struct of the data
+ECHO.ID = echoID;
+ECHO.date = echodate;
+ECHO.volume = volume;
 
 %% DBC data read-out
-filename = 'C:\Users\s165635\Desktop\DBL Computational Biology (8QC00)\OGO groep 5\OGO PSA - DBC data.csv.csv';
+filename = strcat(datalocation, '\OGO PSA - DBC data.csv.csv');
 delimiter = ',';
 startRow = 2;
 
@@ -172,6 +259,15 @@ clearvars filename delimiter startRow formatSpec fileID dataArray ans;
 
 %% Converting of dates to a numeric value
 dbcsdate = datenum(dbcsdate);
+
+% dbcedate completion, applying the date '3000-01-01' to NA values which
+% corresponds to the datenum value: 1095729. This way the date will always
+% be later than any other date in the data set.
+for i=1:length(dbcedate)
+    if strcmp(dbcedate(i,1),'NA') == 1 
+        dbcedate(i,1) = {'3000-01-01'};
+    end
+end
 dbcedate = datenum(dbcedate);
 
 % dbcPCa completion, applying 1 to PCa positve and 0 to PCa negative cases
@@ -183,3 +279,9 @@ for i=1:length(dbcPCa)
     end
 end
 dbcPCa = str2double(dbcPCa);
+
+% Creating a struct of the data
+DBC.ID = dbcID;
+DBC.sdate = dbcsdate;
+DBC.edate = dbcedate;
+DBC.PCa = dbcPCa;
