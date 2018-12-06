d451767e2b994f5864034853662157079ecb7b99
s169977 <s169977@s169977.campus.tue.nl>
2018-12-06 16:05:10 +0100

    Kijken of t lukt

---
+++ Aligning2.m
@@ -0,0 +1,84 @@
+% Aligning of the data
+clear
+close all 
+% Read-out of the data, foldername is the folder where all 5 of the csv 
+% data sets are saved. 
+foldername = 'C:\Users\s169977\Desktop\OGO 5 computational biology\OGO groep 5';
+%[PSA,MRI,BIOPT,ECHO,DBC] = DataReadOut(foldername);
+
+%% Aligning of the data in a new dataset
+Dataset = [];
+for i = 1:length(PSA.ID)
+    pID = PSA.ID(i,1);
+    psaTrue = find(PSA.ID == pID);
+    fpsaTrue = find(PSA.ID == pID);    
+    mriTrue = find(MRI.ID == pID);
+    bioptTrue = find(BIOPT.ID == pID);
+    echoTrue = find(ECHO.ID == pID);
+   
+    if isempty(mriTrue)
+        MRIvalue = 0;
+    else
+        % Currently the script takes the last measured value of a certain
+        % parameter. Another possibility is to average all of the measured
+        % values per patient
+        MRIvalue = MRI.pirads(mriTrue(end),1); 
+    end 
+    
+    if isempty(bioptTrue)
+        bioptvalue = 0;
+    else 
+        bioptvalue = BIOPT.gleason(bioptTrue(end),1);
+    end
+    
+    if isempty(echoTrue) 
+        echovalue = 0;
+    else 
+        echovalue = ECHO.volume(echoTrue(end),1);
+    end
+    
+    Dataset = [Dataset; pID, PSA.psa(psaTrue(end),1), PSA.freepsa(fpsaTrue(end),1), MRIvalue, bioptvalue, echovalue];
+end
+
+% Eliminate the duplicate rows which occur due to multiple PSA level
+% measurements 
+Dataset = unique(Dataset, 'rows');
+
+%% Visualization of data
+% PSA versus PI-RADS score
+subplot(2,2,1);
+imri = find(Dataset(:,4));
+psa = Dataset(imri,2);
+pirads = Dataset(imri,4);
+scatter(psa,pirads)
+axis([0 20 0 6]);
+clearvars psa
+
+% PSA versus free PSA
+subplot(2,2,2);
+ifpsa = find(Dataset(:,3));
+fpsa = Dataset(ifpsa,3);
+psa = Dataset(ifpsa,2);
+scatter(psa,fpsa);
+axis([0 10 0 5]);
+clearvars psa
+
+% PSA versus Gleason score
+subplot(2,2,3);
+igleason = find(Dataset(:,5));
+psa = Dataset(igleason,2);
+gleason = Dataset(igleason,5);
+scatter(psa,gleason);
+axis([0 20 0 10]);
+clearvars psa
+
+% PSA versus Echo volume
+subplot(2,2,4);
+ivolume = find(Dataset(:,6));
+psa = Dataset(ivolume,2);
+volume = Dataset(ivolume,6);
+scatter(psa,volume);
+axis([0 20 0 150]);
+clearvars psa
+
+
--- NumberOfMethods.m
+++ NumberOfMethods.m
@@ -1,32 +1,88 @@
 %get the number of medical examinations and the order of the treatments
 
 %read the dataset
-data=DataReadOut('C:\Users\s169977\Desktop\OGO 5 computational biology\OGO groep 5');
+[PSA,MRI,BIOPT,ECHO,DBC]=DataReadOut('C:\Users\s169977\Desktop\OGO 5 computational biology\OGO groep 5');
 
-maximumID=max(data.ID);
-patientDates=zeros(maximumID, 100);
-NrOfDetections=zeros(maximumID, 1);
+[patientDatesPSA, patientDatesMRI, patientDatesBIOPT, patientDatesECHO, NrOfDetectionsPSA, NrOfDetectionsMRI, NrOfDetectionsBIOPT, NrOfDetectionsECHO]=getPatientDates(PSA,MRI,BIOPT,ECHO,DBC);    
 
-%create a matrix of the dates a examination is done per patientnumber (one
-%row = one patient)
+AllDates=[patientDatesPSA patientDatesMRI patientDatesBIOPT patientDatesECHO];
+AllDates=sort(AllDates);
+AllDates=AllDates(:, 1:100);
+
+maximumID=max(PSA.ID);
+Examinations=zeros(maximumID, 100);
+
 for i=1:maximumID
-    DateNr=find(data.ID==i);
-    NrOfDetections(i)=length(DateNr);
-    dates=sort(data.date(DateNr)');
-    patientDates(i, 1:length(dates))=dates;
+    for j=1:length(AllDates(i,:));
+        date=AllDates(i, j);
+        if ismember(date, patientDatesPSA(i)) && Examinations(i, j)==0;
+            Examinations(i, j)=1; %1 staat voor PSA
+        elseif ismember(date, patientDatesMRI(i)) && Examinations(i, j)==0;
+            Examinations(i, j)=2 && Examinations(i, j)==0; %2 staat voor MRI
+        elseif ismember(date, patientDatesBIOPT(i));
+            Examinations(i, j)=3 && Examinations(i, j)==0; %3 staat voor BIOPT
+        elseif ismember(date, patientDatesECHO(i));
+            Examinations(i, j)= 4 && Examinations(i, j)==0; %4 staat voor ECHO
+        end
+    end
+ 
 end
 
-%find how many examinations are done per patient
-maxNrOfDetections=max(NrOfDetections);
-TotalDetections=zeros(maxNrOfDetections, 1);
-for i=1:maxNrOfDetections
-   TotalDetections(i)=length(find(NrOfDetections==i));
+
+%plot the number of medical examinations used
+maxNrOfDetectionsPSA=max(NrOfDetectionsPSA);
+TotalDetectionsPSA=zeros(maxNrOfDetectionsPSA, 1);
+
+maxNrOfDetectionsMRI=max(NrOfDetectionsMRI);
+TotalDetectionsMRI=zeros(maxNrOfDetectionsMRI, 1);
+
+maxNrOfDetectionsBIOPT=max(NrOfDetectionsBIOPT);
+TotalDetectionsBIOPT=zeros(maxNrOfDetectionsBIOPT, 1);
+
+maxNrOfDetectionsECHO=max(NrOfDetectionsECHO);
+TotalDetectionsECHO=zeros(maxNrOfDetectionsECHO, 1);
+
+for i=1:maxNrOfDetectionsPSA
+   TotalDetectionsPSA(i)=length(find(NrOfDetectionsPSA==i));
 end
 
-%plot the number of examinations done in general
-bar(TotalDetections)
-xlabel('number of medical examinations done on the patient')
-ylabel('number of times a patient this many examinations')
+for i=1:maxNrOfDetectionsMRI
+   TotalDetectionsMRI(i)=length(find(NrOfDetectionsMRI==i));
+end
+
+for i=1:maxNrOfDetectionsBIOPT
+   TotalDetectionsBIOPT(i)=length(find(NrOfDetectionsBIOPT==i));
+end
+
+for i=1:maxNrOfDetectionsECHO
+   TotalDetectionsECHO(i)=length(find(NrOfDetectionsECHO==i));
+end
+
+plotNumberofMethodsUsed(TotalDetectionsPSA, TotalDetectionsMRI, TotalDetectionsBIOPT, TotalDetectionsECHO)
+
+% 
+
+%%extra part I tried to get working but it didn't 
+% classdef Scan
+%     properties
+%         Type
+%         Date
+%         PatientID
+%         Score
+%     end
+% end
+% 
+% classdef Patient
+%     properties
+%         ID
+%         Scans
+%     end
+%     methods 
+%         function r = getScans()
+%             r = Scans;
+%         end
+%     end
+%end
 
 
     
