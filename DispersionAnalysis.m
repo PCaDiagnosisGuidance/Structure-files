86a93cdb394c09d6dd05ffdc055af9ea44bb05a8
RenoDormanns <Reno_dormanns@hotmail.com>
2018-12-11 12:06:59 +0100

    Inclusief size dataset

--- DispersionAnalysis.m
+++ DispersionAnalysis.m
@@ -1,15 +1,16 @@
 function [statsStruct,statsArray]=DispersionAnalysis(input,varargin)
+statsStruct.size=length(input);
 statsStruct.mean=mean(input);
 statsStruct.std=std(input);
 statsStruct.iqr=iqr(input);
 statsStruct.kurtosis=kurtosis(input);
 statsStruct.skewness=skewness(input);
 
-statsArray=[statsStruct.mean; statsStruct.std; statsStruct.iqr; statsStruct.kurtosis; statsStruct.skewness];
+statsArray=[statsStruct.size; statsStruct.mean; statsStruct.std; statsStruct.iqr; statsStruct.kurtosis; statsStruct.skewness];
 
 if strcmp(varargin,'Normplot')
     normplot(input)
-    statType={'mean: ','std: ','iqr: ','kurtosis: ','skewness: '};
+    statType={'size dataset: ','mean: ','std: ','iqr: ','kurtosis: ','skewness: '};
     text(.02,.85,statType,'Units','normalized')
     text(.25,.85,num2str(statsArray),'Units','normalized')
 end
