e7a047e645e278d47d14b6f566ed2a95df3cd9c8
RenoDormanns <Reno_dormanns@hotmail.com>
2018-12-11 15:22:33 +0100

    Nieuwe stuff

---
+++ PSAunder4andOver10Analysis.m
@@ -0,0 +1,78 @@
+clearvars -except BIOPT MRI PSA ECHO DBC
+close all
+
+%[PSA,MRI,BIOPT,ECHO,DBC] = DataReadOut('E:\Scyonite\Documents\MATLAB\OGOPSAdata');
+%[PSA,MRI,BIOPT,ECHO,DBC] = DataReadOut('C:\Users\s129625\Desktop\OGO groep 5');
+
+%%
+
+lowerbound=4;
+upperbound=10;
+
+PSAUniq=unique(PSA.ID);
+
+PSAval=zeros(size(PSAUniq));
+DBCval=PSAval;
+
+for i=1:length(PSAUniq)
+    PSAind=find(PSA.ID==PSAUniq(i));
+    DBCind=find(DBC.ID==PSAUniq(i));
+    
+    PSAperpatient=PSA.psa(PSAind);
+    DBCperpatient=DBC.PCa(DBCind);
+    
+    PSAval(i)=PSAperpatient(1);
+    
+    if isempty(DBCperpatient)
+        DBCval(i) = -1; % geef het een negatieve waarde als de DBC diagnose onbekend is 
+    else 
+        DBCval(i) = DBCperpatient(end);
+    end
+end
+
+U4ind = find(PSAval < lowerbound);
+U4PSAval=PSAval(U4ind);
+U4DBCval=DBCval(U4ind); 
+
+U4pospsa=U4PSAval(U4DBCval==1);
+U4negpsa=U4PSAval(U4DBCval==0);
+U4unknownpsa=U4PSAval(U4DBCval==-1);
+
+figure(1)
+subplot(1,3,1)
+
+U4posStats=DispersionAnalysis(U4pospsa,'Normplot');
+title('Initial PSA values < 4 dataset for patients with PCa')
+subplot(1,3,2)
+
+U4negStats=DispersionAnalysis(U4negpsa,'Normplot');
+title('Initial PSA values < 4 dataset for patients without PCa')
+
+subplot(1,3,3)
+
+U4unknownStats=DispersionAnalysis(U4unknownpsa,'Normplot');
+title('Initial PSA values < 4 dataset for patients without a DBC entry')
+
+O10ind = find(PSAval >= upperbound);
+O10PSAval=PSAval(O10ind);
+O10DBCval=DBCval(O10ind); 
+
+O10pospsa=O10PSAval(O10DBCval==1);
+O10negpsa=O10PSAval(O10DBCval==0);
+O10unknownpsa=O10PSAval(O10DBCval==-1);
+
+figure(2)
+subplot(1,3,1)
+
+O10posStats=DispersionAnalysis(O10pospsa,'Normplot');
+title('Initial PSA values >= 10 dataset for patients with PCa')
+
+subplot(1,3,2)
+
+O10negStats=DispersionAnalysis(O10negpsa,'Normplot');
+title('Initial PSA values >= 10 dataset for patients without PCa')
+
+subplot(1,3,3)
+
+O10unknownStats=DispersionAnalysis(O10unknownpsa,'Normplot');
+title('Initial PSA values >= 10 dataset for patients without a DBC entry')
