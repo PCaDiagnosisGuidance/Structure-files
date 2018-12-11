fa4ebeaeb4b6e839bc157324423211181ecdc6dc
RenoDormanns <Reno_dormanns@hotmail.com>
2018-12-11 19:02:34 +0100

    De functie vereist geen 'Normplot' meer als tweede input. De plot is standaard.

--- DispersionAnalysis.m
+++ DispersionAnalysis.m
@@ -1,3 +1,8 @@
+%Voert analyse uit op dataset input en geeft de grootte van de dataset, het
+%gemiddelde, de standaarddeviatie, de iqr, de Kurtosis waarde (tailedness)
+%en skewness (assymetrie) terug als output. De plot is standaard en vereist
+%geen 'Normplot' als tweede input meer.
+
 function [statsStruct,statsArray]=DispersionAnalysis(input,varargin)
 statsStruct.size=length(input);
 statsStruct.mean=mean(input);
@@ -8,11 +13,16 @@ statsStruct.skewness=skewness(input);
 
 statsArray=[statsStruct.size; statsStruct.mean; statsStruct.std; statsStruct.iqr; statsStruct.kurtosis; statsStruct.skewness];
 
-if strcmp(varargin,'Normplot')
-    normplot(input)
+normplot(input)
     statType={'size dataset: ','mean: ','std: ','iqr: ','kurtosis: ','skewness: '};
     text(.02,.85,statType,'Units','normalized')
     text(.25,.85,num2str(statsArray),'Units','normalized')
-end
+
+% if strcmp(varargin,'Normplot')
+%     normplot(input)
+%     statType={'size dataset: ','mean: ','std: ','iqr: ','kurtosis: ','skewness: '};
+%     text(.02,.85,statType,'Units','normalized')
+%     text(.25,.85,num2str(statsArray),'Units','normalized')
+% end
 
 end
